4d2b2d0b4da79e28334247b00ab584561fa772d2
melonpan <73855155+melonpan@users.noreply.github.com>
2021-03-29 12:15:03 -0300

    finish stage 4 as instructed

--- Zookeeper/task/zookeeper/zookeeper.py
+++ Zookeeper/task/zookeeper/zookeeper.py
@@ -122,4 +122,10 @@ It looks like we will soon have more rabbits!"""
 
 animals = [camel, lion, deer, goose, bat, rabbit]
 
-# write your code here
+while True:
+    user_input = input("Please enter the number of the habitat you would like to view:\n")
+    if user_input != "exit":
+        print(animals[int(user_input)])
+    else:
+        print("See you later!")
+        exit()
